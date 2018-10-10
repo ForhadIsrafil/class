62312504945195b28b5310549dc77badc0434da8
Forhad Israfil <32041831+ForhadIsrafil@users.noreply.github.com>
2018-10-11 03:45:34 +0600

    Update all about class & method.py

--- all about class & method.py
+++ all about class & method.py
@@ -1,24 +1,24 @@
-# # keyword arguments by parameters
-# def positional_argu(name, roll): 
-# 	print(name, roll, sep=' | ')
-# positional_argu(name='forhad', roll=5088) # arguments
+# keyword arguments by parameters
+def positional_argu(name, roll): 
+	print(name, roll, sep=' | ')
+positional_argu(name='forhad', roll=5088) # arguments
 
-# # positional arguments without parameters
-# def positional_argu(name, roll): 
-# 	print(name, roll, sep=' | ')
-# positional_argu('forhad', 5088)
+# positional arguments without parameters
+def positional_argu(name, roll): 
+	print(name, roll, sep=' | ')
+positional_argu('forhad', 5088)
 
-# # arbitrary number of arguments with positional arguments
-# def positional_argu(name, *other_name): # *other_name get & shows result in tuple format
-# 	print(name, other_name, sep=' | ')
-# positional_argu('forhad', 'israfil', 'tuhin', 'raset')
+# arbitrary number of arguments with positional arguments
+def positional_argu(name, *other_name): # *other_name get & shows result in tuple format
+	print(name, other_name, sep=' | ')
+positional_argu('forhad', 'israfil', 'tuhin', 'raset')
 
-# # arbitrary number of arguments with keyword arguments
-# def positional_argu(name, **other_name):             # **other_name get & shows result in dictionary format
-# 	print(name, other_name)
-# positional_argu(name='forhad', second_name='israfil', third_name='tuhin', others_name='raset') # pass argu with key must
-# # can be written in both positional and keyword arguments,here only for 1st parameter is positional
-# positional_argu('forhad', second_name='israfil', third_name='tuhin', others_name='raset') # pass argu with key must
+# arbitrary number of arguments with keyword arguments
+def positional_argu(name, **other_name):             # **other_name get & shows result in dictionary format
+	print(name, other_name)
+positional_argu(name='forhad', second_name='israfil', third_name='tuhin', others_name='raset') # pass argu with key must
+# can be written in both positional and keyword arguments,here only for 1st parameter is positional
+positional_argu('forhad', second_name='israfil', third_name='tuhin', others_name='raset') # pass argu with key must
 
 class A:
 
@@ -43,17 +43,17 @@ class B(A):
 	def sub(self):
 		return self.a - self.b
 
-# In the above example, we have called parent class function as:
-# Person.__init__(self, student_name, student_age) 
-# We can replace this with python super function call as below.
-# super().__init__(student_name, student_age)
+In the above example, we have called parent class function as:
+Person.__init__(self, student_name, student_age) 
+We can replace this with python super function call as below.
+super().__init__(student_name, student_age)
 
-# r = A(7,8)
-# print(r)
-# temp = r.add()
-# print(temp)
-# temp1 = r.mul()
-# print(temp1)
+r = A(7,8)
+print(r)
+temp = r.add()
+print(temp)
+temp1 = r.mul()
+print(temp1)
 
 r1 = B(55,5)
 temp2 = r1.sub()
@@ -62,18 +62,18 @@ temp3 = r1.mul()
 print(temp3)
 
 
-# Polymorphic Classes
-# class Shark():
-#     def swim(self):
-#         print("The shark is swimming.")
-#     def swim_backwards(self):
-#         print("The shark cannot swim backwards, but can sink backwards.")
-#     def skeleton(self):
-#         print("The shark's skeleton is made of cartilage.")
-# class Clownfish():
-#     def swim(self):
-#         print("The clownfish is swimming.")
-#     def swim_backwards(self):
-#         print("The clownfish can swim backwards.")
-#     def skeleton(self):
-#         print("The clownfish's skeleton is made of bon
+Polymorphic/Polymorphism Classes
+class Shark():
+    def swim(self):
+        print("The shark is swimming.")
+    def swim_backwards(self):
+        print("The shark cannot swim backwards, but can sink backwards.")
+    def skeleton(self):
+        print("The shark's skeleton is made of cartilage.")
+class Clownfish():
+    def swim(self):
+        print("The clownfish is swimming.")
+    def swim_backwards(self):
+        print("The clownfish can swim backwards.")
+    def skeleton(self):
+        print("The clownfish's skeleton is made of bon
